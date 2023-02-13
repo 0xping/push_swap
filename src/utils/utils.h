2dfaf43e660b896ebd5ca692650eab4b4517f686
Ahmed Ait Lfdil <aait-lfd@e1r13p9.1337.ma>
2023-02-13 17:37:36 +0100

    add utils functions

---
+++ src/utils/utils.h
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aait-lfd <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/13 07:13:07 by aait-lfd          #+#    #+#             */
+/*   Updated: 2023/02/13 13:13:00 by aait-lfd         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef UTILS_H
+# define UTILS_H
+
+# include <errno.h>
+# include <fcntl.h>
+# include <limits.h>
+# include <stdarg.h>
+# include <stdbool.h>
+# include <stddef.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <strings.h>
+# include <sys/types.h>
+# include <unistd.h>
+# include <wchar.h>
+
+size_t	ft_strlen(const char *str);
+void	ft_bzero(void *s, size_t n);
+int		ft_isspace(int c);
+char	*ft_strdup(const char *s1);
+void	*ft_calloc(size_t count, size_t size);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strtrim(char const *s1, char const *set);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	**ft_split(char const *s, char c);
+int		ft_exit(void);
+
+#endif
